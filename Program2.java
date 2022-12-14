09be6dcb29a67473acaf2292f1c0f45f91bf865e
BananaFlamber <KorolevSteel@inbox.ru>
2022-12-14 21:29:33 +0300

    Second task - halfdone((

--- Program2.java
+++ Program2.java
@@ -1,27 +1,21 @@
+import java.io.File;
 
-import java.io.IOException;
-import java.nio.file.*;
-import java.util.*;
-import java.util.stream.Collectors;
 public class Program2 {
-    public static void main(String[] args) throws IOException {
-        Path directories_info = Paths.get("list.txt");
-        if (!Files.exists(directories_info)) {
-            Files.createFile(directories_info);
-        }
-        Path file = Paths.get("");
-        List<String> str = new ArrayList<>();
-        List<Path> Directories = Files.list(file).collect(Collectors.toList());
-        int size = Directories.size();
-        for (int i = 0; i < size; i++) {
-            Path p = Directories.get(i);
-            str.add(p.toString());
-        }
-        try{
-            Files.write(directories_info,str);}
-        catch(IOException e) {
-        }
 
+    public static void main(String[] args) {
+        File dir = new File("C:\\Users\\annad\\Desktop\\system_log\\Второй блок (Программирование)\\Java\\HomeTaskTwo");
+        if (dir.isDirectory()) {
+
+            for (File item : dir.listFiles()) {
+
+                if (item.isDirectory()) {
+
+                    System.out.println(item.getName() + "  \t folder");
+                } else {
+
+                    System.out.println(item.getName() + "\t file");
+                }
+            }
+        }
     }
-
 }
